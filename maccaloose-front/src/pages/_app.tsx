2f8c6eb5e2bd582d607c0f90c4839a466fabb0aa
mitsu0428 <mitsuprograming@gmail.com>
2023-12-20 20:15:14 +0900

    feat: ここまではプロジェクトテンプレート

--- maccaloose-front/src/pages/_app.tsx
+++ maccaloose-front/src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import * as Css from "@/module/css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Css.ResetCss />
+      <Css.GlobalCss />
+      <Component {...pageProps} />
+    </>
+  );
 }
