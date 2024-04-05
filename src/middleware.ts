c5435e6dad6098f77e054d92e1a327718e8a01ae
Mitsuhiro Okada <okada031717@gmail.com>
2024-04-05 12:24:22 +0900

    1時的なbasicAuth削除

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,21 +5,5 @@ export const config = {
 };
 
 export function middleware(req: NextRequest) {
-  const basicAuth = req.headers.get("authorization");
-
-  if (basicAuth) {
-    const auth = basicAuth.split(" ")[1];
-    const [user, password] = atob(auth).split(":");
-
-    if (user === "macca" && password === "macca") {
-      return NextResponse.next();
-    }
-  }
-
-  return new Response("Auth required", {
-    status: 401,
-    headers: {
-      "WWW-Authenticate": 'Basic realm="Secure Area"',
-    },
-  });
+  // memo: 後ほど修正
 }
